dc01eeddae10b2d4e5c78579d14f84d5e7360076
AnkitGupta <developerplankaro@gmail.com>
2025-12-13 22:49:31 +0530

    Done invoice

---
+++ src/components/Logo.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Logo() {
+  return (
+    <div className="font-bold">
+      {/* <Image
+                src={'/logo.png'}
+                alt="Generate Invoice"
+                width={180}
+                height={50}
+            /> */}
+      RK Gupta Jewellery Shop
+    </div>
+  );
+}
